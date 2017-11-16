f319fd6139a74019d079daecbca0f96d85d6f9dc
Yoghurt <695498118@qq.com>
2017-11-16 23:39:36 +0800

    架构

--- app/index.jsx
+++ app/index.jsx
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import App from './containers/Detail/index.jsx'
-// import { App, store } from './test.jsx'
 import store from './reducers/store.js'
 
 import RouterMap from './router/index.js'
 
-// const element = app;
 
 ReactDOM.render(
     // <App/>,
